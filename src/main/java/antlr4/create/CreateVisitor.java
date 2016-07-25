72fb8fa9aaa188587057160b9333cdb67f39524a
LDL <wsldl123292@163.com>
2016-07-25 22:50:00 +0800

    继续完善create

--- src/main/java/antlr4/create/CreateVisitor.java
+++ src/main/java/antlr4/create/CreateVisitor.java
@@ -1,6 +1,8 @@
 package antlr4.create;
 
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.client.IndicesAdminClient;
+
 import static antlr4.create.CreateParser.*;
 /**
  * 功能:
@@ -8,15 +10,26 @@ import static antlr4.create.CreateParser.*;
  * 时间: 2016-07-21 22:33
  */
 public class CreateVisitor extends CreateParserBaseVisitor<CreateIndexRequestBuilder> {
+
+    private CreateIndexRequestBuilder createIndexRequestBuilder;
+    private IndicesAdminClient indicesAdminClient;
+
+    public CreateVisitor(IndicesAdminClient indicesAdminClient){
+        this.indicesAdminClient = indicesAdminClient;
+    }
+
     @Override
     public CreateIndexRequestBuilder visitShards(ShardsContext ctx) {
-        System.out.println(ctx.getText());
-        return visitChildren(ctx);
+        return createIndexRequestBuilder.setSettings("number_of_shards 1");
     }
     @Override
     public CreateIndexRequestBuilder visitReplicas(ReplicasContext ctx) {
-        System.out.println(ctx.getText());
-        return visitChildren(ctx);
+        return createIndexRequestBuilder.setSettings("number_of_replicas 1");
     }
 
+    @Override
+    public CreateIndexRequestBuilder visitIndex_name(Index_nameContext ctx) {
+        createIndexRequestBuilder = indicesAdminClient.prepareCreate(ctx.getText());
+        return createIndexRequestBuilder;
+    }
 }
--- src/main/java/antlr4/createsql.java
+++ src/main/java/antlr4/createsql.java
@@ -6,6 +6,11 @@ import antlr4.create.CreateVisitor;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 /**
  * 功能:
@@ -14,20 +19,21 @@ import org.antlr.v4.runtime.tree.ParseTree;
  */
 public class createsql {
 
-//    private static final Settings settings = ImmutableSettings.settingsBuilder()
-//                .put("client.transport.sniff", true).put("cluster.name", "es").build();
-//    private static Client client = new TransportClient(settings).addTransportAddress
-//            (new InetSocketTransportAddress("192.168.1.212", 9300));
+    private static final Settings settings = ImmutableSettings.settingsBuilder()
+                .put("client.transport.sniff", true).put("cluster.name", "elasticsearch").build();
+    private static Client client = new TransportClient(settings).addTransportAddress
+            (new InetSocketTransportAddress("192.168.99.100", 9300));
+
     public static void main(String[] args) {
         String sql = "create index A number_of_shards 1 number_of_replicas 1";
-        //client.admin().indices().prepareCreate().setSettings().execute().actionGet();
+
         CreateLexer createLexer = new CreateLexer(new ANTLRInputStream(sql));
         createLexer.removeErrorListeners();
 
         CreateParser parser = new CreateParser(new CommonTokenStream(createLexer));
         ParseTree tree = parser.stat();
 
-        CreateVisitor visitor = new CreateVisitor();
-        visitor.visit(tree);
+        CreateVisitor visitor = new CreateVisitor(client.admin().indices());
+        System.out.println(visitor.visit(tree));
     }
 }
