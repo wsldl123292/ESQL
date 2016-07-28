70a58bb14d449d170e55d52244b5032dba8f8abc
LDL <wsldl123292@163.com>
2016-07-28 21:16:21 +0800

    a

--- src/main/java/com/elasticsearch/mapping/MappingVisitor.java
+++ src/main/java/com/elasticsearch/mapping/MappingVisitor.java
@@ -5,6 +5,6 @@ package com.elasticsearch.mapping;
  * 作者: LDL
  * 日期: 2016/7/26.
  */
-public class MappingVisitor extends MappingParserBaseVisitor {
-
+public class MappingVisitor extends MappingParserBaseVisitor<String> {
+    @Override public String visitIndex_name(MappingParser.Index_nameContext ctx) { return visitChildren(ctx); }
 }
